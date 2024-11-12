2408eaa0532d18d1d7162d466dee470bd51f1efd
finnamini <finnamini2@gmail.com>
2024-11-12 21:01:29 +0100

    multi task fix attempt

--- deep_learning/m1_multi_task.py
+++ deep_learning/m1_multi_task.py
@@ -6,15 +6,26 @@ import os
 import tensorflow as tf
 import datetime
 from keras.saving import register_keras_serializable
+import uuid
 
 
 @register_keras_serializable()
 def custom_age_loss(y_true, y_pred):
-    y_pred = tf.squeeze(y_pred)
+    # print("\n\nSTART")
+    # print("----------age_acc MSE----------")
+    # print("y_true", y_true)
+    # print("y_pred", y_pred)
     mask = tf.not_equal(y_true, -1)
     y_true_adjusted = tf.where(mask, y_true, 0.0)
     y_pred_adjusted = tf.where(mask, y_pred, 0.0)
     mse_loss = tf.square(y_pred_adjusted - y_true_adjusted)
+
+    # print("Mask values (True for valid entries):", mask)
+    # print("y_true_adjusted:", y_true_adjusted)
+    # print("y_pred_adjusted:", y_pred_adjusted.numpy())
+    # print("MSE values:", mse_loss)
+    # print("Reduced mse", tf.reduce_mean(mse_loss))
+    # print("---------------------------")
     return tf.reduce_mean(mse_loss)
 
 
@@ -31,18 +42,24 @@ def custom_gender_loss(y_true, y_pred):
 
 @register_keras_serializable()
 def custom_age_metric(y_true, y_pred):
+    # print("----------age_acc_MAE----------")
+    # print("y_true", y_true)
+    # print("y_pred", y_pred)
+
     y_pred = tf.squeeze(y_pred)
     mask = tf.not_equal(y_true, -1)
     y_true_adjusted = tf.where(mask, tf.cast(y_true, tf.float32), 0.0)
     y_pred_adjusted = tf.where(mask, y_pred, 0.0)
     mae = tf.abs(y_pred_adjusted - y_true_adjusted)
 
-    # print("----------age_acc----------")
+    # print("y_pred_seeezed", y_pred)
     # print("Mask values (True for valid entries):", mask)
     # print("y_true_adjusted:", y_true_adjusted)
     # print("y_pred_adjusted:", y_pred_adjusted.numpy())
     # print("MAE values:", mae)
+    # print("Reduced mae", tf.reduce_mean(mae))
     # print("---------------------------")
+    # print("END\n\n")
 
     return tf.reduce_mean(mae)
 
@@ -67,23 +84,26 @@ if __name__ == "__main__":
 
     top_layer_confs = [
         [
-            Dense(256, activation="relu", name="dense1"),
+            Dense(256, activation="relu", name="dense1.1"),
             Dropout(0.3),
+            Dense(128, activation="relu", name="dense1.2"),
             Dense(1, activation="sigmoid", name="face_detection"),
         ],
         [
-            Dense(256, activation="relu"n name="dense2"),
+            Dense(256, activation="relu", name="dense2.1"),
             Dropout(0.3),
+            Dense(128, activation="relu", name="dense2.2"),
             Dense(1, activation="linear", name="age_prediction"),
         ],
         [
-            Dense(256, activation="relu", name="dense3"),
+            Dense(256, activation="relu", name="dense3.1"),
             Dropout(0.3),
+            Dense(128, activation="relu", name="dense3.2"),
             Dense(1, activation="sigmoid", name="gender_classification"),
         ],
     ]
 
-    model_name = "mt_resnet50_adam_lr=0.0001_lc=0_freeze=True"
+    model_name = f"mt_resnet50_adam_lr=0.0001_lc=0_freeze=True_{uuid.uuid1()}"
     model = build_model_mt(keras_app.ResNet50, (224, 224, 3), top_layer_confs, True)
 
     model.compile(
@@ -98,6 +118,7 @@ if __name__ == "__main__":
             "age_prediction": custom_age_metric,
             "gender_classification": custom_gender_metric,
         },
+        run_eagerly=True,
     )
 
     train_log_dir = (
