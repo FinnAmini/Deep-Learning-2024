d4012ea5b7637339b26bfd305422e94cf2a48e7f
finnamini <finnamini2@gmail.com>
2024-11-14 09:04:16 +0100

    after clone: apply fix changes from yesterday

--- deep_learning/m1_multi_task.py
+++ deep_learning/m1_multi_task.py
@@ -5,79 +5,12 @@ from tensorflow.keras.layers import Dense, Dropout
 import os
 import tensorflow as tf
 import datetime
-from keras.saving import register_keras_serializable
-
-
-@register_keras_serializable()
-def custom_age_loss(y_true, y_pred):
-    # print("\n\nSTART")
-    # print("----------age_acc MSE----------")
-    # print("y_true", y_true)
-    # print("y_pred", y_pred)
-    mask = tf.not_equal(y_true, -1)
-    y_true_adjusted = tf.where(mask, y_true, 0.0)
-    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
-    mse_loss = tf.square(y_pred_adjusted - y_true_adjusted)
-
-    # print("Mask values (True for valid entries):", mask)
-    # print("y_true_adjusted:", y_true_adjusted)
-    # print("y_pred_adjusted:", y_pred_adjusted.numpy())
-    # print("MSE values:", mse_loss)
-    # print("Reduced mse", tf.reduce_mean(mse_loss))
-    # print("---------------------------")
-    return tf.reduce_mean(mse_loss)
-
-
-@register_keras_serializable()
-def custom_gender_loss(y_true, y_pred):
-    mask = tf.not_equal(y_true, -1)
-    y_true_adjusted = tf.where(mask, y_true, 0.0)
-    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
-    binary_crossentropy_loss = tf.keras.losses.binary_crossentropy(
-        y_true_adjusted, y_pred_adjusted
-    )
-    return tf.reduce_mean(binary_crossentropy_loss)
-
-
-@register_keras_serializable()
-def custom_age_metric(y_true, y_pred):
-    # print("----------age_acc_MAE----------")
-    # print("y_true", y_true)
-    # print("y_pred", y_pred)
-
-    y_pred = tf.squeeze(y_pred)
-    mask = tf.not_equal(y_true, -1)
-    y_true_adjusted = tf.where(mask, tf.cast(y_true, tf.float32), 0.0)
-    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
-    mae = tf.abs(y_pred_adjusted - y_true_adjusted)
-
-    # print("y_pred_seeezed", y_pred)
-    # print("Mask values (True for valid entries):", mask)
-    # print("y_true_adjusted:", y_true_adjusted)
-    # print("y_pred_adjusted:", y_pred_adjusted.numpy())
-    # print("MAE values:", mae)
-    # print("Reduced mae", tf.reduce_mean(mae))
-    # print("---------------------------")
-    # print("END\n\n")
-
-    return tf.reduce_mean(mae)
-
-
-@register_keras_serializable()
-def custom_gender_metric(y_true, y_pred):
-    y_pred = tf.squeeze(y_pred)
-    mask = tf.equal(y_true, -1)
-    correct_preds = tf.equal(
-        tf.cast(y_true, tf.int32), tf.cast(tf.round(y_pred), tf.int32)
-    )
-    correct_preds = tf.logical_or(correct_preds, mask)
-    # print("\n\ny_true", y_true)
-    # print("y_pred", y_pred)
-    # print("mask", mask)
-    # print("correct", correct_preds)
-    # print("res", tf.reduce_mean(tf.cast(correct_preds, tf.float32)), "\n")
-    return tf.reduce_mean(tf.cast(correct_preds, tf.float32))
-
+from train import (
+    custom_age_loss,
+    custom_age_metric,
+    custom_gender_loss,
+    custom_gender_metric,
+)
 
 if __name__ == "__main__":
     train_ds, val_ds = load_dataset_from_directory(
@@ -192,7 +125,6 @@ if __name__ == "__main__":
                     "age_prediction": custom_age_metric,
                     "gender_classification": custom_gender_metric,
                 },
-                run_eagerly=True,
             )
 
             train_log_dir = f"logs/mt/{model_name}_{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
--- deep_learning/train.py
+++ deep_learning/train.py
@@ -9,6 +9,94 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.optimizers import Adam
 from keras.saving import register_keras_serializable
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.losses import binary_crossentropy
+
+
+@register_keras_serializable()
+def custom_age_loss(y_true, y_pred):
+    mask = tf.not_equal(y_true, -1)
+    y_true_adjusted = tf.where(mask, y_true, 0.0)
+    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
+    mse_loss = tf.square(y_pred_adjusted - y_true_adjusted)
+    loss = tf.reduce_mean(mse_loss)
+
+    if tf.executing_eagerly():
+        print("\n----------AGE LOSS----------")
+        print("y_true", y_true)
+        print("y_pred", y_pred)
+        print("mask:", mask)
+        print("y_true_adjusted:", y_true_adjusted)
+        print("y_pred_adjusted:", y_pred_adjusted)
+        print("mse loss values:", mse_loss)
+        print("mse batch loss:", loss)
+        print("---------------------------\n")
+    return loss
+
+
+@register_keras_serializable()
+def custom_gender_loss(y_true, y_pred):
+    mask = tf.not_equal(y_true, -1)
+    y_true_adjusted = tf.where(mask, y_true, 0.0)
+    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
+    bce_loss = binary_crossentropy(y_true_adjusted, y_pred_adjusted)
+    loss = tf.reduce_mean(bce_loss)
+
+    if tf.executing_eagerly():
+        print("\n----------GENDER LOSS----------")
+        print("y_true", y_true)
+        print("y_pred", y_pred)
+        print("mask:", mask)
+        print("y_true_adjusted:", y_true_adjusted)
+        print("y_pred_adjusted:", y_pred_adjusted)
+        print("binary cross entropy:", bce_loss)
+        print("bce batch loss:", loss)
+        print("---------------------------\n")
+    return loss
+
+
+@register_keras_serializable()
+def custom_age_metric(y_true, y_pred):
+    y_pred = tf.squeeze(y_pred)
+    mask = tf.not_equal(y_true, -1)
+    y_true_adjusted = tf.where(mask, tf.cast(y_true, tf.float32), 0.0)
+    y_pred_adjusted = tf.where(mask, y_pred, 0.0)
+    mae = tf.abs(y_pred_adjusted - y_true_adjusted)
+    loss = tf.reduce_mean(mae)
+
+    if tf.executing_eagerly():
+        print("\n----------AGE METRIC----------")
+        print("y_true", y_true)
+        print("y_pred", y_pred)
+        print("mask:", mask)
+        print("y_true_adjusted:", y_true_adjusted)
+        print("y_pred_adjusted:", y_pred_adjusted)
+        print("mae loss values:", mae)
+        print("age batch loss:", loss)
+        print("---------------------------\n")
+
+    return loss
+
+
+@register_keras_serializable()
+def custom_gender_metric(y_true, y_pred):
+    y_pred = tf.squeeze(y_pred)
+    mask = tf.equal(y_true, -1)
+    correct_preds = tf.equal(
+        tf.cast(y_true, tf.int32), tf.cast(tf.round(y_pred), tf.int32)
+    )
+    correct_preds = tf.logical_or(correct_preds, mask)
+    acc = tf.reduce_mean(tf.cast(correct_preds, tf.float32))
+
+    if tf.executing_eagerly():
+        print("\n----------GENDER METRIC----------")
+        print("y_true", y_true)
+        print("y_pred", y_pred)
+        print("mask:", mask)
+        print("correction preds:", correct_preds)
+        print("gender batch acc:", acc)
+        print("---------------------------\n")
+
+    return acc
 
 
 def create_dataset(
@@ -359,11 +447,15 @@ def eval(model: str, images: str, labels: str):
     if labels is not None:
         model.compile(
             optimizer=Adam(learning_rate=0.0001),
-            loss=multi_task_loss,
+            loss=[
+                tf.keras.losses.BinaryCrossentropy(),
+                custom_age_loss,
+                custom_gender_loss,
+            ],
             metrics={
                 "face_detection": "accuracy",
-                "age_prediction": "mae",
-                "gender_classification": "accuracy",
+                "age_prediction": custom_age_metric,
+                "gender_classification": custom_gender_metric,
             },
         )
     else:
