a206271b8b8d941e0743972c338ad95c25607701
Paddy369 <mayer.patrick97@paddyshomelab.de>
2025-01-16 13:47:59 +0100

    Update android frontend app

--- MindenCameraApp_2025/app/src/main/java/com/example/mindencameraapp/MainActivity.java
+++ MindenCameraApp_2025/app/src/main/java/com/example/mindencameraapp/MainActivity.java
@@ -13,6 +13,7 @@ package com.example.mindencameraapp;
 import androidx.annotation.NonNull;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
@@ -23,6 +24,8 @@ import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.media.Image;
 import android.media.ImageReader;
+import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 import android.util.Size;
 import androidx.appcompat.app.AppCompatActivity;
@@ -67,7 +70,9 @@ import org.tensorflow.lite.support.label.TensorLabel;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
@@ -77,6 +82,21 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Query;
 
 
 // ----------------------------------------------------------------------
@@ -104,12 +124,7 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
     };
 
     private final Object task = new Object();
-
-    private Interpreter tflite;
-    // add your filename here (label names)
-    final String CLASSIFIER_LABEL_File = "labels_model_new4_1.txt";
     // add your filename here (model file)
-    final String TF_LITE_File = "model_student4_3.flite";
     List<String> clasifierLabels = null;
 
 
@@ -127,14 +142,14 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
         buttonTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                captureImage();
+                captureImage(false);
             }
         });
         buttonGallery = findViewById(R.id.buttonGallery);
         buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveImage();
+                captureImage(true);
             }
         });
 
@@ -184,23 +199,6 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
     @SuppressLint("RestrictedApi")
     private void startCameraX(ProcessCameraProvider cameraProvider) {
 
-        // load label file
-        try {
-            clasifierLabels = FileUtil.loadLabels(this, CLASSIFIER_LABEL_File);
-        } catch (IOException e) {
-            Log.e("startCameraX", "Error reading label file", e);
-        }
-        // load tf lite model
-        try{
-            MappedByteBuffer tfliteModel;
-            tfliteModel = FileUtil.loadMappedFile(this,TF_LITE_File);
-            tflite = new Interpreter(tfliteModel);
-
-        } catch (IOException e){
-            Log.e("startCameraX", "Error reading model", e);
-        }
-
-
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
                 .build();
@@ -233,21 +231,23 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
 
     // ----------------------------------------------------------------------
     // capture single image
-    private void captureImage() {
+    private void captureImage(Boolean recognize) {
         imageCapture.takePicture(getExecutor(), new ImageCapture.OnImageCapturedCallback() {
             @Override
             public void onCaptureSuccess(@NonNull ImageProxy image) {
                 super.onCaptureSuccess(image);
                 Log.d("TAG", "Capture Image");
-                classifySingleImage(image);
+                sendImageToBackend(image, recognize);
             }
         });
     }
 
 
     // ----------------------------------------------------------------------
-    // save image to file
-    private void saveImage() {
+    // recognize image
+    private void recognizeImage() {
+        Log.d("Debug", "Pressing button 2...");
+
         long timeStamp = System.currentTimeMillis();
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timeStamp);
@@ -276,11 +276,9 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
     }
 
     // ----------------------------------------------------------------------
-    // classify single image
-    private void classifySingleImage(@NonNull ImageProxy imageProxy) {
-        Log.d("classifySingleImage", "CLASSIFY_IMAGE " + imageProxy.getImageInfo().getTimestamp());
-
-        Log.d("analyze", "format " + imageProxy.getFormat());
+    // send iamge to backend
+    private void sendImageToBackend(@NonNull ImageProxy imageProxy, Boolean recognize) {
+        Log.d("Debug", "Pressing button 1...");
 
         ByteBuffer buffer = imageProxy.getPlanes()[0].getBuffer();
         byte[] arr = new byte[buffer.remaining()];
@@ -288,77 +286,137 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
         Bitmap bitmapImage = BitmapFactory.decodeByteArray(arr, 0, buffer.capacity());
         imageProxy.close();
 
-        int rotation = imageProxy.getImageInfo().getRotationDegrees();
-
-        imageProxy.close();
         int width  = bitmapImage.getWidth();
         int height = bitmapImage.getHeight();
-        Log.d("classifySingleImage", "(width,height): " + width + " " + height);
 
-        // image size set to 256x256 (use bilinear interpolation)
+        // image size set to 224x224 (use bilinear interpolation)
         int size = height > width ? width : height;
         ImageProcessor imageProcessor = new ImageProcessor.Builder()
                 .add(new Rot90Op(1))
                 .add(new ResizeWithCropOrPadOp(size, size))
-                .add(new ResizeOp(256, 256, ResizeOp.ResizeMethod.BILINEAR))
+                .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
                 .build();
 
         TensorImage tensorImage = new TensorImage(DataType.FLOAT32);
         tensorImage.load(bitmapImage);
         tensorImage = imageProcessor.process(tensorImage);
-        TensorBuffer probabilityBuffer =
-                TensorBuffer.createFixedSize(new int[]{1, 15}, DataType.FLOAT32);
 
-        if(null != tflite) {
-            tflite.run(tensorImage.getBuffer(), probabilityBuffer.getBuffer());
+        // Convert processed TensorImage back to Bitmap
+        Bitmap processedBitmap = tensorImage.getBitmap();
+
+        // Save the processed image locally
+        if (!recognize) {
+            saveImageToLocalStorage(processedBitmap);
         }
-        TensorProcessor probabilityProcessor =
-                new TensorProcessor.Builder()/*.add(new NormalizeOp(0, 255))*/.build();
 
-        String resultString = " ";
-        if (null != clasifierLabels) {
-            // Map of labels and their corresponding probability
-            TensorLabel labels = new TensorLabel(clasifierLabels,
-                    probabilityProcessor.process(probabilityBuffer));
+        // Send the image to the backend
+        sendImage(processedBitmap, recognize);
+    }
 
-            // Create a map to access the result based on label
-            Map<String, Float> floatMap = labels.getMapWithFloatValue();
-            resultString = getResultString(floatMap);
-            Log.d("classifySingleImage", "RESULT: " + resultString);
-            Toast.makeText(MainActivity.this, resultString, Toast.LENGTH_SHORT).show();
+    private void saveImageToLocalStorage(Bitmap bitmap) {
+        FileOutputStream outStream = null;
+        try {
+            // Get the Pictures directory
+            File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            if (!storageDir.exists()) {
+                storageDir.mkdirs();
+            }
+
+            // Create a file for the processed image
+            String fileName = "processed_image_" + System.currentTimeMillis() + ".png";
+            File imageFile = new File(storageDir, fileName);
+
+            // Write the bitmap to the file
+            outStream = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(imageFile);
+            mediaScanIntent.setData(contentUri);
+            sendBroadcast(mediaScanIntent);
+
+            Log.d("Debug", "Image saved to: " + imageFile.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e("Debug", "Error saving image", e);
+        } finally {
+            if (outStream != null) {
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    Log.e("Debug", "Error closing output stream", e);
+                }
+            }
         }
     }
 
+    // Method to send the image to the backend
+    private void sendImage(Bitmap bitmap, Boolean recognize) {
+        // Convert Bitmap to File
+        File file = createTempFileFromBitmap(bitmap);
 
-    private Bitmap yuvImageToBitmap(Image image) {
-        // input image is in yuv-format
-        Image.Plane[] planes = image.getPlanes();
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
+        if (file != null) {
+            // Create Retrofit instance
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://yourbackend.com/") // Replace with your backend URL
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
+            // Get API service
+            ApiService apiService = retrofit.create(ApiService.class);
 
-        byte[] nv21 = new byte[ySize + uSize + vSize];
-        //U and V are swapped
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-        uBuffer.get(nv21, ySize + vSize, uSize);
+            // Create RequestBody and MultipartBody.Part
+            RequestBody requestBody = RequestBody.create(MediaType.parse("image/png"), file);
+            MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), requestBody);
 
-        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+            // Make the API call
+            Call<ResponseBody> call = apiService.uploadImage(body, recognize);
+            Log.d("Debug", "Image enqueue");
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(@NonNull Call<ResponseBody> call, @NonNull Response<ResponseBody> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        Log.d("Debug", "Image uploaded successfully");
+                    } else {
+                        Log.e("Debug", "Failed to upload image: " + (response.errorBody() != null ? response.errorBody().toString() : "Unknown error"));
+                    }
+                }
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 90, out);
+                @Override
+                public void onFailure(@NonNull Call<ResponseBody> call, @NonNull Throwable t) {
+                    Log.e("Debug", "Error uploading image", t);
+                }
+            });
+        } else {
+            Log.d("Debug", "File is null");
+        }
+    }
 
-        byte[] imageBytes = out.toByteArray();
-        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    public interface ApiService {
+        @Multipart
+        @POST("api/recognize")
+        Call<ResponseBody> uploadImage(
+                @Part MultipartBody.Part file,
+                @Query("recognize") boolean recognize
+        );
     }
 
 
-    // ----------------------------------------------------------------------
-    // process current frame
+    // Helper method to convert Bitmap to a temporary file
+    private File createTempFileFromBitmap(Bitmap bitmap) {
+        try {
+            File tempFile = File.createTempFile("processed_image", ".png");
+            FileOutputStream outStream = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+            outStream.flush();
+            outStream.close();
+            return tempFile;
+        } catch (IOException e) {
+            Log.e("Debug", "Error creating temp file", e);
+            return null;
+        }
+    }
+
     @Override
     public void analyze(@NonNull ImageProxy imageProxy) {
         if ( imageProxy.getFormat()== PixelFormat.RGBA_8888){
@@ -385,9 +443,6 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
             TensorBuffer probabilityBuffer =
                     TensorBuffer.createFixedSize(new int[]{1, 15}, DataType.FLOAT32);
 
-            if(null != tflite) {
-                tflite.run(tensorImage.getBuffer(), probabilityBuffer.getBuffer());
-            }
             TensorProcessor probabilityProcessor =
                     new TensorProcessor.Builder()/*.add(new NormalizeOp(0, 255))*/.build();
 
